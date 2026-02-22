f9445d5c038440cb1712c5a1a2d45940bf86b272
expln <expln@>
2026-02-22 08:38:36 +0100

    Fixed `can't access property "onClicked", browser.action is undefined`.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 const POPUP_URL = browser.runtime.getURL("popup.html");
 
-browser.action.onClicked.addListener(async () => {
+browser.browserAction.onClicked.addListener(async () => {
   await browser.windows.create({
     url: POPUP_URL,
     type: "popup",
